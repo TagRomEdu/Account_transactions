74b40062af0d22ddc28676589610897e08dfe604
TagRomEdu <qwertystyles@gmail.com>
2023-07-03 15:12:54 +0500

    settings.py is fixed

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 from os import path
 
-OPERATIONS_PATH = path.join('app', 'fixture', 'operations.json')
+OPERATIONS_PATH = path.join('/', '0_programming_python', 'course_work_3', 'app', 'fixture', 'operations.json')
 
+print(OPERATIONS_PATH)
