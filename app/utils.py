2ec9fdc9493e1e1af0f734de564abe4b4bd8828d
TagRomEdu <qwertystyles@gmail.com>
2023-07-03 17:20:26 +0500

    utils.py is updated with three functions

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 from settings import OPERATIONS_PATH
 
 
@@ -11,12 +12,35 @@ def get_operations(path) -> list[dict]:
 
 
 def get_executed_ops(ops_lst: list[dict]) -> list[dict]:
+    """
+    Исключаем из списка операций те, что не прошли.
+    """
     executed_list = [dictionary for dictionary in ops_lst if dictionary.get('state') == 'EXECUTED']
     return executed_list
 
 
-def sort_list(ops_lst: list[dict]) -> list[dict]:
-    return sorted(ops_lst, key=lambda operation: operation['date'], reverse=True)
+def sort_list_of_5(ops_lst: list[dict]) -> list[dict]:
+    """
+    Сортируем наш список операций по дате от последней и возвращаем первые 5 позиций
+    """
+    return sorted(ops_lst, key=lambda operation: operation['date'], reverse=True)[:5]
 
-"""a = get_operations(OPERATIONS_PATH)
-print(sort_list(get_executed_ops(a))[0:5])"""
+
+def convert_date(str_date: str) -> str:
+    data = datetime.strptime(str_date, '%Y-%m-%dT%H:%M:%S.%f')
+    return datetime.strftime(data, '%d.%m.%Y')
+
+
+def convert_num(str_data: str) -> str:
+    lst = str_data.split(' ')
+    if len(lst[-1]) == 16:
+        a = lst[-1][:4]
+        b = lst[-1][4:6]
+        c = lst[-1][-4:]
+        return f"{a} {b}** **** {c}"
+    else:
+        return f"**{lst[-1][-4:]}"
+
+
+print(convert_num(a))
+
